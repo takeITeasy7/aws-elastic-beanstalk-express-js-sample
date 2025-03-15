b4b44f8a9bc6ebcc87e485a1fe112e1bb47692db
takeITeasy7 <tattsuya.7@gmail.com>
2025-03-15 18:28:22 +0900

    change message 3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Welcome to AWS DevOps! and this is my first AWS project. I am so excited to learn AWS DevOps!'));
+app.get('/', (req, res) => res.send('Hello World! Welcome to AWS DevOps! and this is my first AWS project. I am so excited to learn AWS DevOps! auto deploy!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
